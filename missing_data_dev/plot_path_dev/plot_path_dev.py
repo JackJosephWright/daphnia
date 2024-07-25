071921994471915455fa1802e1d11a635ccc4b1d
irahat21 <IbrahimRahat150@gmail.com>
2024-07-25 13:34:15 -0400

    Update plot_path_dev.py

--- missing_data_dev/plot_path_dev/plot_path_dev.py
+++ missing_data_dev/plot_path_dev/plot_path_dev.py
@@ -8,9 +8,9 @@ from ..npz_zip_dev import zip
 import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
-npz_path = r"C:\Users\jwright\Documents\GitHub\daphnia\data\npz_file\small_100_fish0.npz"
-
-
+#npz_path = r"C:\Users\jwright\Documents\GitHub\daphnia\data\npz_file\small_100_fish0.npz"
+#npz_path = r"/Users/ibrahimrahat/Documents/GitHub/daphnia/data/npz_file/small_100_fish0.npz"
+npz_path = r"/Users/ibrahimrahat/Documents/GitHub/daphnia/data/npz_file/single_7_9_fish1.MP4_fish0.npz"
 
 #use np.load to load the npz file
 data = np.load(npz_path)
@@ -22,6 +22,26 @@ timestamp = data['timestamp']
 X = data['X']
 Y = data['Y']
 
+df = pd.DataFrame(
+    {
+        "X": X,
+        "Y": Y,
+        "time": timestamp
+    }
+)
+
+#plt.plot(X, Y)
+#plt.show()
+#print(df)
 
 
-#get these all into a dataframe
+def plotDetail(title, xlable, ylabel):
+    plt.title(title)
+    plt.xlabel(xlable)
+    plt.ylabel(ylabel)
+
+plt.plot(X, Y)
+plotDetail("NPZ","X value","Y value")
+plt.show()
+
+#if __name__ == "__main__":
