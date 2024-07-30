86bf60b79def9e720f40b9ded7a43470ebe4bbd4
irahat21 <IbrahimRahat150@gmail.com>
2024-07-30 14:22:51 -0400

    Update test_plotDetail.py

--- tests/test_plotDetail.py
+++ tests/test_plotDetail.py
@@ -8,9 +8,9 @@ from src.data_manipulation.plotDetail import plotDetail
 import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
-npz_path = r"C:\Users\jwright\Documents\GitHub\daphnia\data\npz_file\small_100_fish0.npz"
+#npz_path = r"C:\Users\jwright\Documents\GitHub\daphnia\data\npz_file\small_100_fish0.npz"
 #npz_path = r"/Users/ibrahimrahat/Documents/GitHub/daphnia/data/npz_file/small_100_fish0.npz"
-# npz_path = r"/Users/ibrahimrahat/Documents/GitHub/daphnia/data/npz_file/single_7_9_fish1.MP4_fish0.npz"
+npz_path = r"/Users/ibrahimrahat/Documents/GitHub/daphnia/data/npz_file/single_7_9_fish1.MP4_fish0.npz"
 
 #use np.load to load the npz file
 data = np.load(npz_path)
@@ -21,7 +21,6 @@ timestamp = data['timestamp']
 
 X = data['X']
 Y = data['Y']
-Y = -Y
 
 df = pd.DataFrame(
     {
