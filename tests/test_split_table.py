c1f4be7c9afb9df90783e2c7a0849e6627a00305
irahat21 <IbrahimRahat150@gmail.com>
2024-07-31 13:50:02 -0400

    added data to test

--- tests/test_split_table.py
+++ tests/test_split_table.py
@@ -1,3 +1,8 @@
-from  missing_data_dev.max_velocity.check_velocity import split_table
+from  missing_data_dev.max_velocity.split_table_dev import split_table
+import pandas as pd
 
-print(split_table)
+Clean_data = r"/Users/ibrahimrahat/Documents/GitHub/daphnia/data/clean_fish_data/fish_data_clean.csv"
+
+df = pd.read_csv(Clean_data)
+
+print(df)
