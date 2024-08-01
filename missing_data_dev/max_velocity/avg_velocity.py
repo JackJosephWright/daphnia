b454f6481f373e05afbd65bb607a1a76d2733c94
irahat21 <IbrahimRahat150@gmail.com>
2024-08-01 15:33:46 -0400

    Update avg_velocity.py

--- missing_data_dev/max_velocity/avg_velocity.py
+++ missing_data_dev/max_velocity/avg_velocity.py
@@ -17,18 +17,54 @@ for file in all_files:
     # print(file_path)
     df = pd.read_csv(file_path)
     dataframes.append(df)
-    
-# first_dataframe = dataframes[0]
+    # break
+
+first_dataframe = dataframes[0]
 # print(first_dataframe)
 
-x = df['X']
-y = df['Y']
-time = df['time']
+
+def calc_avg_velocity(df):
+
+    x = df['X']
+    y = df['Y']
+    time = df['time']
+    time = time / 100000
+    # differences in positions (displacements)
+    delta_x = x.diff().iloc[1:]
+    delta_y = y.diff().iloc[1:]
+    # print(delta_x)
+    # print(delta_y)
+    
+    # Euclidean distance (total displacement)
+    displacement = np.sqrt(delta_x**2 + delta_y**2)
+    # print(displacement)
+
+    # differences in time
+    delta_time = time.diff().iloc[1:]
+    # print(delta_time)
+
+    # instantaneous velocities
+    velocity = displacement / delta_time
+    # print(velocity)
+
+    avg_velocity = velocity.mean()
+
+    return avg_velocity
+
+
+# calculate average velocities in all dataframes
+avg_velocities = [calc_avg_velocity(df) for df in dataframes]
+"wont calculate avg_velocity for tables with one value of data, most likely bc of iloc[1:]"
+
+# loop thru and print it
+# for i, avg_velo in enumerate(avg_velocities):
+    # print(f"Average Velocity for file {i+1}: {avg_velo:.2f}")
+
+
+
+# print(calc_avg_velocity(df))
 
 
 
 
-
-
-# for i in dataframes:
 
