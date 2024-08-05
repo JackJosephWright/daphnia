e1134c9160ba97d72efaa7a0aa7d21a08b3b4c6f
irahat21 <IbrahimRahat150@gmail.com>
2024-08-05 10:36:24 -0400

    added test file for avg_velocity and saved old code in avg_velocity2

---
+++ tests/test_avg_velocity.py
@@ -0,0 +1,9 @@
+import pytest
+import numpy as np
+# Import classes from src/data_manipulation
+# from src.data_manipulation.NPZer import NPZer
+# from src.data_manipulation.TRexDataTester import TRexDataTester
+
+from missing_data_dev.max_velocity.avg_velocity import avg_velocity
+
+print(avg_velocity())
