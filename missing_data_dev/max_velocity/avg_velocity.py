63a4e94f0aa1c3e04196d10a37f7a79b36946704
irahat21 <IbrahimRahat150@gmail.com>
2024-08-05 11:36:04 -0400

    updated avg_velocity

--- missing_data_dev/max_velocity/avg_velocity.py
+++ missing_data_dev/max_velocity/avg_velocity.py
@@ -5,13 +5,39 @@ import numpy as np
 # from data import table_data
 
 # directory containing csv table files
-direc_path = "/Users/ibrahimrahat/Documents/GitHub/daphnia/data/table_data"
+direct_path = "/Users/ibrahimrahat/Documents/GitHub/daphnia/data/table_data"
 
-all_files = os.listdir(direc_path)
+all_files = os.listdir(direct_path)
 
 dataframes = []
 
-def avg_velocity():
-    pass
-    print('inside avg velocity funciton')
-    return 10
+def calc_velocity(point1, point2):
+    print("point 1:", point1)
+    print("point 2:", point2)
+    # use the pythagorean theorem to calculate the displacement
+    delta_x = point2["X"] - point1["X"]
+    delta_y = point2["Y"] - point1["Y"]
+    print("delta_x: ", delta_x, "delta_y: ", delta_y)
+    displacement = np.sqrt(delta_x**2 + delta_y**2)
+    print("displacement:", displacement)
+    # calculate the difference in time (delta)
+    delta_time = point2["time"] - point1["time"]
+    print("delta_time:", delta_time)
+    # divide the displacement by the difference in time
+    velocity = displacement / delta_time
+    
+    # return the velocity
+    return velocity
+
+
+#write a function that 
+     
+def avg_velocity(df):
+    V = []
+    for i in len(df) + 1:
+    point2 = df(i)
+    point1 = df(i - 1)
+    V.append(calc_velocity(point1, point2))
+
+
+    
--- tests/test_avg_velocity.py
+++ tests/test_avg_velocity.py
@@ -1,9 +1,30 @@
 import pytest
 import numpy as np
+import os
+import pandas as pd
 # Import classes from src/data_manipulation
 # from src.data_manipulation.NPZer import NPZer
 # from src.data_manipulation.TRexDataTester import TRexDataTester
 
-from missing_data_dev.max_velocity.avg_velocity import avg_velocity
+from missing_data_dev.max_velocity.avg_velocity import calc_velocity, avg_velocity
+# directory containing csv table files
+direct_path = "/Users/ibrahimrahat/Documents/GitHub/daphnia/data/table_data"
 
-print(avg_velocity())
+all_files = os.listdir(direct_path)
+
+dataframes = []
+
+# loop through csv files and read each one 
+for file in all_files:
+    file_path = os.path.join(direct_path, file)
+    # print(file_path)
+    df = pd.read_csv(file_path)
+    dataframes.append(df)
+    # break
+
+first_dataframe = dataframes[0]
+# print(first_dataframe)
+p1 = first_dataframe.iloc[0]
+p2 = first_dataframe.iloc[1]
+print('first df :', first_dataframe)
+print(calc_velocity(p1,p2))
