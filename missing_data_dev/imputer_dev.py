2252314958a2f8d4be2abb0ee1c30351e3a1fcb3
IcyCreme <96668603+GiulianoV8@users.noreply.github.com>
2024-08-08 12:45:54 -0400

    fixed some bugs

--- missing_data_dev/imputer_dev.py
+++ missing_data_dev/imputer_dev.py
@@ -8,10 +8,10 @@ import pandas as pd
 impute = avgValue.impute
 
 cols = ['X', 'Y', 'time']
-#fishDataPandas = NPZer().pandafy(source_dir = 'data/npz_file/single_7_9_fish1.MP4_fish0.npz', invertY = True, params = cols)
-fishDataPandas = pd.read_csv('data/clean_fish_data/fish_data_clean.csv')
-fishDataPandas.drop(columns = ['Unnamed: 0'], inplace=True)
-#fishDataPandas, _ = TRexDataCleaner().renderDiscontinuities(data = fishDataPandas, vmax = 40)
+fishDataPandas = NPZer().pandafy(source_dir = 'data/npz_file/single_7_9_fish1.MP4_fish0.npz', invertY = True, params = cols)
+#fishDataPandas = pd.read_csv('data/clean_fish_data/fish_data_clean.csv')
+#fishDataPandas.drop(columns = ['Unnamed: 0'], inplace=True)
+fishDataPandas, _ = TRexDataCleaner().renderDiscontinuities(data = fishDataPandas, vmax = 1.4*(10**-5))
 
 fishDataPandas = impute(data = fishDataPandas)
 
