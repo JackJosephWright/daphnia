0c32faec80d8edc2fa30c4c1c2fd42233c8436cd
Jack Wright <jwrightzz1234@gmail.com>
2024-08-08 10:24:17 -0400

    added velocity histogram to the formatted report

--- output/email_update_single_imputer/image_generation_files/avg_velocity_plots.py
+++ output/email_update_single_imputer/image_generation_files/avg_velocity_plots.py
@@ -11,4 +11,19 @@ from missing_data_dev.max_velocity.avg_velocity import calc_velocity, avg_veloci
 relative_path = "data/table_data"
 all_files = os.listdir(relative_path)
 
-print(all_files)
+# Create an empty dataframe
+dataframes = []
+
+# Loop through and have pandas read each CSV file
+for file in all_files:
+    file_path = os.path.join(relative_path, file)
+    df = pd.read_csv(file_path)
+    dataframes.append(df)
+
+
+
+# Use the function all_velocity and store all the velocities into a variable
+all_velo = all_velocity(dataframes)
+
+# Plot the velocities in a histogram
+plot_histogram(all_velo)
