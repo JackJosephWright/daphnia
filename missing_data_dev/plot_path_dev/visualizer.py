03eb173605bcef368c23ee75335cc0968a5e2ea7
irahat21 <IbrahimRahat150@gmail.com>
2024-08-16 19:38:06 -0400

    Update visualizer.py

--- missing_data_dev/plot_path_dev/visualizer.py
+++ missing_data_dev/plot_path_dev/visualizer.py
@@ -4,7 +4,28 @@ import numpy as np
 from matplotlib.animation import FuncAnimation
 
 class DaphniaAnimation:
+    """ Animates the movement of a Daphnia using clean data from an NPZ file
+
+        Functions:
+        ----------
+        __init__:
+            Initializes the DaphniaAnimation class with the given NPZ file and start index
+        load_data:
+            Loads data from NPZ file into dataframes and removes missing data
+        plot_detail:
+            Sets the title and labels for the plot
+        init_animation:
+            Initializes the animation by setting the line data to empty
+        animate:
+            Updates the animation frame by frame
+        create_animation:
+            Creates and displays the animation of the Daphnia's movements
+
+    """
     def __init__(self, npz_path, start_index=0):
+        """ Initializes the DaphniaAnimation class with the given NPZ file and start index
+
+        """
         self.npz_path = npz_path
         self.start_index = start_index
         self.df = self.load_data()
@@ -14,6 +35,9 @@ class DaphniaAnimation:
         self.line, = self.ax.plot([], [], 'b-')
 
     def load_data(self):
+        """ Loads data from NPZ file into dataframes and removes missing data
+
+        """
         # Load the npz file
         data = np.load(self.npz_path)
         timestamp = data['timestamp']
@@ -34,6 +58,9 @@ class DaphniaAnimation:
         return df
 
     def plot_detail(self, title, xlabel, ylabel):
+        """ Sets the title and labels for the plot
+        
+        """
         csfont = {'fontname': 'Comic Sans MS', 'color': 'blue', 'size': 20}
         hfont = {'fontname': 'Helvetica', 'color': 'blue', 'size': 12}
         plt.title(title, fontdict=csfont)
@@ -41,16 +68,25 @@ class DaphniaAnimation:
         plt.ylabel(ylabel, fontdict=hfont)
 
     def init_animation(self):
+        """ Initializes the animation by setting the line data to empty
+
+        """
         self.line.set_data([], [])
         return self.line,
 
     def animate(self, i):
+        """ Updates the animation frame by frame
+        
+        """
         x = self.df_subset['X'][:i]
         y = self.df_subset['Y'][:i]
         self.line.set_data(x, y)
         return self.line,
 
     def create_animation(self):
+        """ Creates and displays the animation of the Daphnia's movements
+        
+        """
         # Set up plot limits
         self.ax.set_xlim(self.df_subset['X'].min(), self.df_subset['X'].max())
         self.ax.set_ylim(self.df_subset['Y'].min(), self.df_subset['Y'].max())
