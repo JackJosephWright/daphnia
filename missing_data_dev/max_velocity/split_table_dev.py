8b8f6c2736a9b66a2a6b40a87bd089a8a64b84db
irahat21 <IbrahimRahat150@gmail.com>
2024-08-19 00:47:19 -0400

    made a parameter that saves the tables all into a folder

--- missing_data_dev/max_velocity/split_table_dev.py
+++ missing_data_dev/max_velocity/split_table_dev.py
@@ -5,6 +5,7 @@ save tables as dataframes
 
 import pandas as pd
 import numpy as np
+import os
 
 
 clean_data = r"/Users/ibrahimrahat/Documents/GitHub/daphnia/data/clean_fish_data/fish_data_clean.csv"
@@ -12,7 +13,7 @@ clean_data = r"/Users/ibrahimrahat/Documents/GitHub/daphnia/data/clean_fish_data
 df = pd.read_csv(clean_data, index_col=False)
 
 
-def split_table(df):
+def split_table(df, save_to_folder=False, folder_path=None):
     #remove unwanted columns
     for col in df.columns:
         print(col)
@@ -37,13 +38,20 @@ def split_table(df):
                 temp_table.loc[len(temp_table)] = row.tolist()
     if temp_table is not None:
         tables.append(temp_table)
-                
+
+
+     # Save tables to folder if specified
+    if save_to_folder and folder_path:
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        save_tables(tables, folder_path)
+
     return tables
 
 
-def save_tables(tables):
+def save_tables(tables, folder_path):
     for index, table in enumerate(tables):
-        table.to_csv(f'table_{index}.csv', index=False)
+        table.to_csv(os.path.join(folder_path, f'table_{index}.csv'), index=False)
 
 
 
--- tests/test_split_table.py
+++ tests/test_split_table.py
@@ -1,4 +1,4 @@
-from  missing_data_dev.max_velocity.split_table_dev import split_table, save_tables
+from  missing_data_dev.max_velocity.split_table_dev import split_table
 import pandas as pd
 
 # clean_data = r"data/npz_file/single_7_9_fish1.MP4_fish0.npz"
@@ -6,8 +6,14 @@ clean_data = r"/Users/ibrahimrahat/Documents/GitHub/daphnia/data/clean_fish_data
 
 df = pd.read_csv(clean_data)
 
+# set save_to_folder to True if you want to save to folder
+tables = split_table(df, save_to_folder=False, folder_path='output_folder')
 
-tables = split_table(df)
-print(tables)
+# index of what table you want to access
+first_table = tables[0]
+print(first_table)
 
-# save_tables(tables)
+# number of tables
+num_tables = len(tables)
+print(num_tables)
+
