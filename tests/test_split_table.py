834a8dcc9aa21f4358c02d96a5cae0a007474995
IcyCreme <96668603+GiulianoV8@users.noreply.github.com>
2024-08-21 15:10:42 -0400

    testing splut table

--- tests/test_split_table.py
+++ tests/test_split_table.py
@@ -1,12 +1,19 @@
-from  missing_data_dev.max_velocity.split_table_dev import split_table ,save_tables
+from  missing_data_dev.max_velocity.split_table_dev import split_table
 import pandas as pd
 
-Clean_data = r"/Users/ibrahimrahat/Documents/GitHub/daphnia/data/clean_fish_data/fish_data_clean.csv"
+# clean_data = r"data/npz_file/single_7_9_fish1.MP4_fish0.npz"
+clean_data = "data/clean_fish_data/fish_data_clean.csv"
 
-df = pd.read_csv(Clean_data)
+df = pd.read_csv(clean_data)
 
+# set save_to_folder to True if you want to save to folder
+tables = split_table(df, save_to_folder=False, folder_path='output_folder')
 
-tables = split_table(df)
+# index of what table you want to access
+first_table = tables[0]
+print(first_table)
 
+# number of tables
+num_tables = len(tables)
+print(num_tables)
 
-save_tables(tables)
