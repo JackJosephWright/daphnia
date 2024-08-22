676ba2b3ddcd8b174f8e28a71f6158f2cb73eaab
irahat21 <IbrahimRahat150@gmail.com>
2024-08-22 17:43:40 -0400

    Update avg_velocity.py

--- missing_data_dev/max_velocity/avg_velocity.py
+++ missing_data_dev/max_velocity/avg_velocity.py
@@ -9,26 +9,48 @@ all_files = os.listdir(relative_path)
 dataframes = []
 
 def calc_velocity(point1, point2):
-    """"""
+    """
+    Calculates the velocity between two points based on their X, Y coordinates and time
 
+    Parameters:
+    -----------
+    point1 : pd.Series
+        The first point containing 'X', 'Y', and 'time' values
+    point2 : pd.Series
+        The second point containing 'X', 'Y', and 'time' values
+
+    Returns:
+    --------
+    float
+        The calculated velocity between the two points
+    """
     delta_x = point2["X"] - point1["X"]
     delta_y = point2["Y"] - point1["Y"]
-    #print("delta_x: ", delta_x, "delta_y: ", delta_y)
+
     displacement = np.sqrt(delta_x**2 + delta_y**2)
-    #print("displacement:", displacement)
 
     delta_time = point2["time"] - point1["time"]
-    #print("delta_time:", delta_time)
 
     velocity = displacement / delta_time
     
     return velocity
 
 
-     
 def avg_velocity(df):
+    """
+    Calculates the average velocity between consecutive points in a DataFrame
+
+    Parameters:
+    -----------
+    df : pd.DataFrame
+        A DataFrame containing 'X', 'Y', and 'time' columns
+
+    Returns:
+    --------
+    list
+        A list of velocities between consecutive points in the DataFrame
+    """
     V = []
-    #print('length of df:',len(df))
     for i in range(1, len(df)):
         point2 = df.iloc[i]
         point1 = df.iloc[i - 1]
@@ -36,9 +58,20 @@ def avg_velocity(df):
     return V
 
 
-#collect all the velocities and put in one vector
-
 def all_velocity(df_list):
+    """
+    Collects all velocities from a list of DataFrames and returns them as a single list
+
+    Parameters:
+    -----------
+    df_list : list
+        A list of DataFrames, each containing 'X', 'Y', and 'time' columns
+
+    Returns:
+    --------
+    list
+        A list of all velocities calculated from the DataFrames
+    """
     all_v = []
     for df in df_list:
         v_vector = avg_velocity(df)
@@ -47,13 +80,20 @@ def all_velocity(df_list):
     
 
 
-#plot the velocities as a continuous density function (or histogram)
-
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 def plot_histogram(data, bins=10):
+    """
+    Plots a histogram of the given data, representing the distribution of velocities.
+
+    Parameters:
+    -----------
+    data : list
+        A list of velocity values to be plotted.
+    bins : int, optional
+        The number of bins to use in the histogram (default is 10).
+    """
     plt.figure(figsize=(8, 6))
     plt.hist(data, bins=bins, edgecolor='black', alpha=0.7)
     plt.xlabel('Velocity')
