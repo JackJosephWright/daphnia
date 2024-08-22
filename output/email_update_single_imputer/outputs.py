7d31970a8f9478ca1bec99a9542baa425c5c7346
IcyCreme <96668603+GiulianoV8@users.noreply.github.com>
2024-08-22 10:05:47 -0400

    collecting images for documentation

--- output/email_update_single_imputer/outputs.py
+++ output/email_update_single_imputer/outputs.py
@@ -1,21 +1,23 @@
 # Import necessary tools
 import pandas as pd
 import numpy as np
-from src.data_manipulation.NPZer import NPZer
 import matplotlib.pyplot as plt
 
+
+from src.data_manipulation.NPZer import NPZer
 
 NPZer = NPZer()
 
 # Set desired parameters
 SOURCE_DIR = 'data/npz_file/single_7_9_fish1.MP4_fish0.npz'
-INVERT_Y = True
 PARAMS = ['time', 'X', 'Y']
+INVERT_Y = True
 
 # Unzip and turn data into a pandas table
-unzippedData = NPZer.pandafy(source_dir=SOURCE_DIR,
-                              invertY=INVERT_Y,
-                              params=PARAMS)
+unzippedData = NPZer.pandafy(source_dir=SOURCE_DIR, invertY=INVERT_Y, params=PARAMS)
+
+# Unzip to nparray
+unzippedData = NPZer.unzipNpz(source_dir=SOURCE_DIR, params=PARAMS)
 
 # Print data in form of pandas table
 print('TRex Data:\n', unzippedData)
@@ -34,6 +36,12 @@ originalData = unzippedData.reset_index(drop=True)
 # Print sample of original data
 print('Original Data:\n', originalData)
 
+originalData = pd.DataFrame({
+    'X': [1.0, 2.0, 10.0, 4.0],
+    'Y': [1.0, 2.0, 10.0, 4.0],
+    'time': [0.0, 1.0, 2.0, 3.0]
+})
+
 # Clean data
 cleanedData, removedData = dataCleaner.renderDiscontinuities(data=originalData, vmax=VMAX)
 
@@ -46,16 +54,50 @@ from src.data_manipulation.TRexImputer import TRexImputer
 imputer = TRexImputer()
 
 # Set desired parameters
-DATA = cleanedData
+DATA = pd.DataFrame({
+    'X': [1.0, 2.0, np.inf, 4.0],
+    'Y': [1.0, 2.0, np.inf, 4.0],
+    'time': [0.0, 1.0, 2.0, 3.0]
+})
 FUNCTION = 'avgValue'
-
-# Print original data
-print('Original Data:\n', cleanedData)
 
 # Impute data
 imputedData = imputer.impute(data=DATA, function=FUNCTION)
+
+
+# Print original data
+print('Original Data:\n', cleanedData)
 
 # Print imputed data
 print('Imputed Data:\n', imputedData)
 plt.plot(imputedData['X'], imputedData['Y'])
 plt.show()
+
+
+
+
+
+import pandas as pd
+from src.data_manipulation.NPZer import NPZer
+
+DATA = pd.DataFrame({
+    'X': [1, 2, 3, 4],
+    'Y': [1, 2, 3, 4],
+    'time': [0, 1, 2, 3]
+})
+SAVE_DIR = 'data/zipped.npz'
+PARAMS = ['X', 'Y', 'time']
+
+npzer = NPZer()
+npzer.npzip(data=DATA, save_dir=SAVE_DIR, params=PARAMS)
+
+from src.data_manipulation.calculateVelocity import calculateVelocity
+import pandas as pd
+
+data = pd.DataFrame({
+    'X': [1, 2, 3, 4],
+    'Y': [1, 2, 3, 4],
+    'time': [0, 1, 2, 3]
+})
+
+calculateVelocity(pi=data.loc[0], pf=data.loc[1])
