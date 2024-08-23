5a24bd47c2608c1b7c25d75ff7c10a2c737eae6a
irahat21 <IbrahimRahat150@gmail.com>
2024-08-23 12:34:58 -0400

    renamed

--- missing_data_dev/plot_path_dev/example_code.py
+++ missing_data_dev/plot_path_dev/example_code.py
@@ -27,10 +27,9 @@ animation.create_animation()
 
 
 
-from  missing_data_dev.max_velocity.split_table_dev import split_table, save_tables
+from  missing_data_dev.max_velocity.split_table_dev import split_table
 import pandas as pd
 
-# clean_data = r"data/npz_file/single_7_9_fish1.MP4_fish0.npz"
 clean_data = r"data/clean_fish_data/fish_data_clean.csv"
 
 df = pd.read_csv(clean_data)
@@ -38,11 +37,11 @@ df = pd.read_csv(clean_data)
 # set save_to_folder to True if you want to create a folder of tables in explorer 
 tables = split_table(df, save_to_folder=False, folder_path='output_folder')
 
-# index of what table you want to access
+# can choose index of what table you want to access
 first_table = tables[0]
 print(first_table)
 
-# number of tables
+# prints number of tables
 num_tables = len(tables)
 print(num_tables)
 
@@ -54,8 +53,7 @@ print(num_tables)
 import numpy as np
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
-from missing_data_dev.max_velocity.avg_velocity import calc_velocity, avg_velocity, all_velocity, plot_histogram
+from missing_data_dev.max_velocity.avg_velocity import all_velocity, plot_histogram
 
 # directory containing csv table files
 direct_path = r'data/table_data'
@@ -69,8 +67,8 @@ for file in all_files:
     df = pd.read_csv(file_path)
     dataframes.append(df)
 
-
+# calculates velocities of all tables
 all_velo = all_velocity(dataframes)
 
-
+# plots histogram of all calculated velocities
 plot_histogram(all_velo)
