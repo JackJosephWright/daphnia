77dfdafc8df578c3c0665326a106d1db3c3b5b91
IcyCreme <96668603+GiulianoV8@users.noreply.github.com>
2024-08-26 15:28:29 -0400

    Update calculateVelocity.py

--- src/data_manipulation/calculateVelocity.py
+++ src/data_manipulation/calculateVelocity.py
@@ -3,19 +3,20 @@ import pandas as pd
 
 def calculateVelocity(pi: pd.DataFrame, pf: pd.DataFrame) -> float:
     """
-        Calculates the velocity between two points.
+    Calculates the velocity between two points.
+    
+    Parameters:
+    -----------
+    pi : DataFrame
+        The initial point of the entity, given as a section of a pandas DataFrame.
+    pf : DataFrame
+        The final position of the entity, given as a section of a pandas DataFrame.
 
-        Parameters:
-        -----------
-        pi : DataFrame
-            The initial point of the entity, given as a section of a pandas DataFrame.
-        pf : DataFrame
-            The final position of the entity, given as a section of a pandas DataFrame.
-        Returns:
-        --------
-        float
-            The calculated velocity.
-        """
+    Returns:
+    --------
+    float
+        The calculated velocity.
+    """
     dx = pf["X"] - pi["X"]
     dy = pf["Y"] - pi["Y"]
 
